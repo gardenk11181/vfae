488daf4ae04da58e140e2f2c8dec3f9912dc15ab
Jeongwon Kim <55073876+gardenk11181@users.noreply.github.com>
2023-02-18 13:39:50 +0900

    Complete basic setting

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import find_packages, setup
 setup(
     name="src",
     version="0.0.1",
-    description="Describe Your Cool Project",
-    author="",
+    description="VFAE",
+    author="Jeongwon Kim",
     author_email="",
     url="https://github.com/user/project",  # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
     install_requires=["pytorch-lightning", "hydra-core"],
