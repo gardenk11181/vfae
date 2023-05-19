6ac83f6d8dacce32b517e4a7fa0530ec9809f718
Jeongwon Kim <55073876+gardenk11181@users.noreply.github.com>
2023-05-19 10:11:17 +0900

    add script file for german dataset experiment

---
+++ scripts/test_german.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+dataset="german"
+version=$1
+
+> logs/$dataset/$version.log
+
+for i in 1 2 3 4 5; do 
+	echo "${i}th dataset" >> logs/$dataset/$version.log
+	python src/eval.py +experiment=$dataset/$version \
+	domain=$i | egrep 'acc_y' >> logs/$dataset/$version.log
+done
